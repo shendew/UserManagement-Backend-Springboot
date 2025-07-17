3b991b528e5226d3be5eca52970be5709c2b3441
Shehara Dewanagala <sahokavishka@gmail.com>
2025-07-17 23:40:42 +0530

    initial commit

---
+++ src/test/java/com/example/user_namagment/UserNamagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_namagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNamagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
